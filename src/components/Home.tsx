1ed1ff4368c6f2e5e1c12085f4392bf5d725bc7b
Ledditz <drlucas97@gmail.com>
2022-07-18 14:11:06 +0200

    add loading indicator

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Box, Stack } from '@mui/material';
+import { Box, CircularProgress, Stack, Typography } from '@mui/material';
 import SearchBar from './SearchBar';
 import BuyDialog from './userCard/BuyDialog';
 import RechargeDialog from './userCard/RechargeDialog';
 import UserCard from './userCard/UserCard';
 import { useNavigate } from 'react-router-dom';
 import { useColorMode } from '../ColorModeContext';
-import { user } from '../hooks/useFetchUsers';
+import { useFetchUsers, user } from '../hooks/useFetchUsers';
 
 interface HomeProps {
     onLogoutClick: () => void
@@ -21,31 +21,13 @@ export interface rechargeObj {
     rechargeAmount: number | 'new'
 }
 
-const userDataMock: user[] = [
-    { name: "Hans Wurst", amount: 1.5 },
-    { name: "Max Mustermann", amount: 10 },
-    { name: "R. Satzmann", amount: 6.5 },
-    { name: "Spritt Kanne", amount: 0.5 },
-    { name: "Karl Gustav", amount: 1.5 },
-    { name: "Hinz", amount: 10 },
-    { name: "Kunz", amount: 6.5 },
-    { name: "Max", amount: 0.5 },
-    { name: "Moritz", amount: 1.5 },
-    { name: "Dings Bums", amount: 10 },
-    { name: "R. Satzmann", amount: 6.5 },
-    { name: "Spritt Kanne", amount: 0.5 },
-    { name: "Hans Günther", amount: 1.5 },
-    { name: "Max Mustermann", amount: 10 },
-    { name: "R. Satzmann", amount: 6.5 },
-    { name: "Spritt Kanne", amount: 0.5 }
-]
-
 const Home = (props: HomeProps) => {
     const { onLogoutClick } = props
     const [isExpanded, setIsExpanded] = useState<number>(-1)
     const [dialogOpen, setDialogOpen] = useState<string>('')
     const [drinkData, setDrinkData] = useState<any>({})
     const [rechargeData, setRechargeData] = useState<any>({})
+    const { users, isLoading, error } = useFetchUsers()
     const [filteredUserData, setFilteredUserData] = useState<user[]>([])
     const [sortedUsers, setSortedUsers] = useState<user[]>([])
     const { mode } = useColorMode()
@@ -57,6 +39,10 @@ const Home = (props: HomeProps) => {
         setSortedUsers(users.sort((a, b) => a.name > b.name ? 1 : -1))
 
     }, [filteredUserData])
+
+    useEffect(() => {
+        setFilteredUserData(users)
+    }, [users])
 
     const handleExpandChange = (idx: number) => {
         setIsExpanded(idx === isExpanded ? -1 : idx)
@@ -87,10 +73,10 @@ const Home = (props: HomeProps) => {
     const handleInputChange = (_term: string) => {
         let newData: user[] = []
         if (_term === '') {
-            newData = userDataMock
+            newData = users
         } else {
             const lowerCaseSearchTerm = _term.toLowerCase()
-            newData = userDataMock.filter(user => {
+            newData = users.filter(user => {
                 const lowerCaseUserName = user.name.toLowerCase();
                 return lowerCaseUserName.indexOf(lowerCaseSearchTerm) !== -1
             })
@@ -100,9 +86,24 @@ const Home = (props: HomeProps) => {
         // console.log(_term)
     }
 
-    return (
-        <Box bgcolor={mode === 'light' ? "lightgrey" : "grey"}>
-            <SearchBar onInputChange={handleInputChange} onLogoutClick={handleLogoutClick} />
+    const showContent = () => {
+        if (isLoading)
+            return (
+                <Box sx={{ display: 'flex', justifyContent: 'center', width: '100vw', height: '90vh', alignItems: 'center' }}>
+                    <CircularProgress size='4rem' />
+                </Box>
+            )
+
+        if (error)
+            return (
+                <Box sx={{ display: 'flex', justifyContent: 'center', width: '100vw', height: '90vh', alignItems: 'center' }}>
+                    <Typography color={mode === 'light' ? 'black' : 'white'}>
+                        Oh Oh da hat was nicht funktioniert..
+                    </Typography>
+                </Box>
+            )
+
+        return (
             <Stack direction="column" alignItems="flex-start" justifyContent="center" marginTop={8} paddingTop={2}>
                 {sortedUsers.map((user, idx) =>
                     <UserCard
@@ -114,8 +115,18 @@ const Home = (props: HomeProps) => {
                         expandedIdx={isExpanded}
                         onBuyClick={handleBuyClick}
                         onRechargeClick={handleRechargeClick}
-                    />)}
+                    />
+                )}
             </Stack>
+        )
+    }
+
+
+    return (
+        <Box bgcolor={mode === 'light' ? "lightgrey" : "grey"}>
+            <SearchBar onInputChange={handleInputChange} onLogoutClick={handleLogoutClick} />
+            {showContent()}
+
             {dialogOpen === 'buy' && <BuyDialog open onClose={handleCloseDialog} drinksData={drinkData} />}
             {dialogOpen === 'recharge' && <RechargeDialog open onClose={handleCloseDialog} rechargeObj={rechargeData} />}
         </Box>
--- src/hooks/useFetchUsers.tsx
+++ src/hooks/useFetchUsers.tsx
@@ -5,15 +5,38 @@ export interface user {
     amount: number
 }
 
+const userDataMock: user[] = [
+    { name: "Hans Wurst", amount: 1.5 },
+    { name: "Max Mustermann", amount: 10 },
+    { name: "R. Satzmann", amount: 6.5 },
+    { name: "Spritt Kanne", amount: 0.5 },
+    { name: "Karl Gustav", amount: 1.5 },
+    { name: "Hinz", amount: 10 },
+    { name: "Kunz", amount: 6.5 },
+    { name: "Max", amount: 0.5 },
+    { name: "Moritz", amount: 1.5 },
+    { name: "Dings Bums", amount: 10 },
+    { name: "R. Satzmann", amount: 6.5 },
+    { name: "Spritt Kanne", amount: 0.5 },
+    { name: "Hans Günther", amount: 1.5 },
+    { name: "Max Mustermann", amount: 10 },
+    { name: "R. Satzmann", amount: 6.5 },
+    { name: "Spritt Kanne", amount: 0.5 }
+]
+
 export const useFetchUsers = () => {
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(true)
     const [users, setUsers] = useState<user[]>([])
     const [error, setError] = useState(false)
 
     useEffect(() => {
         // hier nach den usern fetchen 
         // error handling
-
+        setTimeout(() => {
+            setLoading(false)
+            // setError(true)
+            setUsers(userDataMock)
+        }, 500)
     }, [])
 
     return {
